6a193e1bfcce67631bdaca3973040ee50302eaaf
Marjorie Ivy <macivymorenomt@users.noreply.github.com>
2021-02-19 11:37:18 -0600

    finalized day three

--- Day Three/day3.py
+++ Day Three/day3.py
@@ -17,14 +17,7 @@ print(new_shopping_list)
 new_shopping_list.pop(-1)
 print(new_shopping_list)
 new_shopping_list.append('Blue Paint')
-print(new_shopping_list)
-new_shopping_list.append('Blue Paint')
-print(new_shopping_list)
-new_shopping_list.append('Blue Paint')
-print(new_shopping_list)
-new_shopping_list.append('Blue Paint')
-print(new_shopping_list)
-new_shopping_list.append('Blue Paint')
+new_shopping_list.extend(['Blue Paint','Blue Paint','Blue Paint','Blue Paint',])
 print(new_shopping_list)
 new_shopping_list.pop(0)
 new_shopping_list.insert(0, "20 x Maple Plank")
@@ -32,8 +25,7 @@ print(new_shopping_list)
 new_shopping_list.pop(1)
 new_shopping_list.insert(1, "20 x Maple Plank")
 print(new_shopping_list)
-new_shopping_list = ['20 x Maple Plank', '20 x Maple Plank', '20 x Cherry Plank', 'White Paint',
-                   'Blue Paint','Blue Paint','Blue Paint','Blue Paint', 'Blue Paint']
+
 # Solution
 paint_list = new_shopping_list[3:]
 print(paint_list)
